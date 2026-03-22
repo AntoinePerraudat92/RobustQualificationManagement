867db15241316bcf3564d035c2adaf82ed322e58
AntoinePerraudat <antoine.perraudat@planimize.com>
2026-03-22 20:33:43 +0100

    draft

--- test/test_MasterProblem.py
+++ test/test_MasterProblem.py
@@ -51,3 +51,25 @@ class MyTestCase(TestCase):
         self.assertAlmostEqual(5.0, master_problem.get_qualification_costs())
         self.assertAlmostEqual(1.0, master_problem.get_qualification_decision(product=0, factory=0))
         self.assertAlmostEqual(0.0, master_problem.get_qualification_decision(product=1, factory=0))
+
+    def test_simple_problem_with_two_scenarios(self):
+        nmb_products = 2
+        nmb_factories = 1
+        qualification_matrix = np.array([[1], [1]], dtype=np.float64)
+        qualification_costs = np.array([[1], [1]], dtype=np.float64)
+        lost_sales_cost = np.array([50, 50], dtype=np.float64)
+        factory_capacities = np.array([30], dtype=np.float64)
+        dataset: Dataset = Dataset(nmb_products, nmb_factories, qualification_matrix, qualification_costs, lost_sales_cost, factory_capacities)
+        first_demand_scenario: DemandScenario = DemandScenario(product_demands=np.array([15, 0], dtype=np.float64))
+        second_demand_scenario: DemandScenario = DemandScenario(product_demands=np.array([0, 15], dtype=np.float64))
+
+        master_problem: MasterProblem = MasterProblem(dataset)
+        master_problem.add_scenario(demand_scenario=first_demand_scenario)
+        master_problem.add_scenario(demand_scenario=second_demand_scenario)
+
+        master_problem.solve()
+
+        self.assertAlmostEqual(0.0, master_problem.get_lost_sales(scenario=0))
+        self.assertAlmostEqual(2.0, master_problem.get_qualification_costs())
+        self.assertAlmostEqual(1.0, master_problem.get_qualification_decision(product=0, factory=0))
+        self.assertAlmostEqual(1.0, master_problem.get_qualification_decision(product=1, factory=0))
