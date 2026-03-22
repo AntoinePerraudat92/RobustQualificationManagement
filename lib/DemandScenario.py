e42dba66288fcbc9f5482862357f572bb606a889
AntoinePerraudat <antoine.perraudat@planimize.com>
2026-03-22 20:03:00 +0100

    draft

--- lib/DemandScenario.py
+++ lib/DemandScenario.py
@@ -3,7 +3,21 @@ from numpy.typing import NDArray
 
 from dataclasses import dataclass
 
+from lib.DemandUncertaintySet import DemandUncertaintySet
+
 
 @dataclass
 class DemandScenario:
     product_demands: NDArray[np.float64]
+
+
+def generate_random_demand_scenario(nmb_products: int, seed: int, uncertainty_set: DemandUncertaintySet) -> DemandScenario:
+    local_random = np.random.default_rng(seed)
+    product_demands = [
+        local_random.uniform(uncertainty_set.demand_lower_bounds[p], uncertainty_set.demand_upper_bounds[p]) for p
+        in range(nmb_products)]
+    product_demands = np.array(product_demands, dtype=np.float64)
+    total_demand = np.sum(product_demands)
+    return DemandScenario(product_demands=uncertainty_set.maximum_total_demand / total_demand * product_demands)
+
+
---
+++ test/test_DemandScenario.py
@@ -0,0 +1,22 @@
+from unittest import TestCase
+
+import numpy as np
+
+from lib.DemandScenario import generate_random_demand_scenario
+from lib.DemandUncertaintySet import DemandUncertaintySet
+
+
+class Test(TestCase):
+
+    def test_generate_random_demand_scenario(self):
+        nmb_products = 3
+        seed = 0
+        uncertainty_set: DemandUncertaintySet = DemandUncertaintySet(
+            demand_lower_bounds=np.array([0, 15, 10], dtype=np.float64),
+            demand_upper_bounds=np.array([0, 25, 40], dtype=np.float64),
+            maximum_total_demand=np.float64(40.0)
+        )
+
+        demand_scenario = generate_random_demand_scenario(nmb_products, seed, uncertainty_set)
+
+        self.assertAlmostEqual(uncertainty_set.maximum_total_demand, np.sum(demand_scenario.product_demands))
