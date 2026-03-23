f433f0b64f49dd7434a5ba3f4a639896cc92b79e
AntoinePerraudat <antoine.perraudat@planimize.com>
2026-03-23 18:41:59 +0100

    inlined var

--- lib/MasterProblem.py
+++ lib/MasterProblem.py
@@ -37,27 +37,18 @@ class MasterProblem:
         self.eta = self.model.addVariable(obj=1.0)
         for product in range(dataset.nmb_products):
             for factory in range(dataset.nmb_factories):
-                self.add_qualification_variables(product=product, factory=factory,
-                                                 obj=float(self.dataset.qualification_costs[product][factory]))
-
-    def add_qualification_variables(self, product: int, factory: int, obj: float) -> None:
-        self.qualification_variables[self.QualificationVariable(product=product, factory=factory)] = self.model.addBinary(
-            obj=obj)
-
-    def add_lost_sales_variable(self, product: int) -> None:
-        self.lost_sales_variables[
-            self.LostSalesVariable(scenario=self.scenario, product=product)] = self.model.addVariable()
-
-    def add_workload_variable(self, product: int, factory: int) -> None:
-        self.workload_variables[
-            self.WorkloadVariable(scenario=self.scenario, product=product, factory=factory)] = self.model.addVariable()
+                obj = float(self.dataset.qualification_costs[product][factory])
+                self.qualification_variables[self.QualificationVariable(product=product, factory=factory)] = self.model.addBinary(
+                    obj=obj)
 
     def add_scenario(self, demand_scenario: DemandScenario) -> None:
         # Variables.
         for product in range(self.dataset.nmb_products):
             for factory in range(self.dataset.nmb_factories):
-                self.add_workload_variable(product=product, factory=factory)
-            self.add_lost_sales_variable(product=product)
+                self.workload_variables[
+                    self.WorkloadVariable(scenario=self.scenario, product=product, factory=factory)] = self.model.addVariable()
+            self.lost_sales_variables[
+                self.LostSalesVariable(scenario=self.scenario, product=product)] = self.model.addVariable()
 
         # Flow constraints.
         for factory in range(self.dataset.nmb_factories):
--- lib/RecourseProblem.py
+++ lib/RecourseProblem.py
@@ -35,18 +35,14 @@ class RecourseProblem:
         self.workload_variables = {}
         self.lost_sales_variables = {}
 
-    def add_lost_sales_variable(self, product: int, obj: float) -> None:
-        self.lost_sales_variables[self.LostSalesVariable(product=product)] = self.model.addVariable(obj=obj)
-
-    def add_workload_variable(self, product: int, factory: int) -> None:
-        self.workload_variables[self.WorkloadVariable(product=product, factory=factory)] = self.model.addVariable()
-
     def build(self, qualification_matrix: NDArray[np.int64], demand_scenario: DemandScenario):
         # Variables.
         for product in range(self.dataset.nmb_products):
             for factory in range(self.dataset.nmb_factories):
-                self.add_workload_variable(product=product, factory=factory)
-            self.add_lost_sales_variable(product=product, obj=float(self.dataset.lost_sales_costs[product]))
+                self.workload_variables[
+                    self.WorkloadVariable(product=product, factory=factory)] = self.model.addVariable()
+            obj = float(self.dataset.lost_sales_costs[product])
+            self.lost_sales_variables[self.LostSalesVariable(product=product)] = self.model.addVariable(obj=obj)
 
         # Flow constraints.
         for factory in range(self.dataset.nmb_factories):
