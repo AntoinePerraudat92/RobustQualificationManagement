20604e482783011acad6c7f135a17ffa101e0e67
AntoinePerraudat <antoine.perraudat@planimize.com>
2026-03-23 17:38:32 +0100

    added test

--- test/test_RecourseProblem.py
+++ test/test_RecourseProblem.py
@@ -40,3 +40,20 @@ class Test(TestCase):
         recourse_problem.solve()
 
         self.assertAlmostEqual(40.0, recourse_problem.get_lost_sales())
+
+
+    def test_simple_problem_with_two_product_and_one_factory(self):
+        nmb_products = 2
+        nmb_factories = 1
+        qualification_matrix = np.array([[1], [1]], dtype=np.float64)
+        qualification_costs = np.array([[1], [1]], dtype=np.float64)
+        lost_sales_cost = np.array([10, 30], dtype=np.float64)
+        factory_capacities = np.array([25], dtype=np.float64)
+        dataset: Dataset = Dataset(nmb_products, nmb_factories, qualification_matrix, qualification_costs, lost_sales_cost, factory_capacities)
+        demand_scenario: DemandScenario = DemandScenario(product_demands=np.array([5, 40], dtype=np.float64))
+
+        recourse_problem: RecourseProblem = RecourseProblem(dataset)
+        recourse_problem.build(qualification_matrix, demand_scenario)
+        recourse_problem.solve()
+
+        self.assertAlmostEqual(500.0, recourse_problem.get_lost_sales())
