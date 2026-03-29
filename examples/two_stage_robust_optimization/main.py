69e4a85dbca5e88e8355c5a257a768704c9e0754
AntoinePerraudat <antoine.perraudat@planimize.com>
2026-03-29 10:00:16 +0200

    remove file

--- examples/two_stage_robust_optimization/main.py
+++ examples/two_stage_robust_optimization/main.py
@@ -8,9 +8,9 @@ from src.data_model.DemandUncertaintySet import DemandUncertaintySet
 
 def main(seed: int):
     # Problem dimension.
-    nmb_scenarios = 100
-    nmb_products = 300
-    nmb_factories = 12
+    nmb_scenarios = 500
+    nmb_products = 30
+    nmb_factories = 5
 
     # Generate demand.
     local_rng = np.random.default_rng(seed=seed)
@@ -28,9 +28,9 @@ def main(seed: int):
     qualification_matrix = np.array(local_rng.uniform(0.0, 1.0, size=(nmb_products, nmb_factories,)) <= 0.50,
                                     dtype=np.int64)
     qualification_costs = local_rng.uniform(1.0, 50.0, size=(nmb_products, nmb_factories,))
-    lost_sales_cost = local_rng.uniform(10.0, 1000.0, size=(nmb_products,))
-    total_demand_per_factory = (maximum_total_demand * nmb_products) / nmb_factories
-    factory_capacities = local_rng.uniform(total_demand_per_factory * 0.90, total_demand_per_factory, size=(nmb_factories,))
+    lost_sales_cost = local_rng.uniform(1.0, 10000.0, size=(nmb_products,))
+    total_demand_per_factory = maximum_total_demand / nmb_factories
+    factory_capacities = local_rng.uniform(total_demand_per_factory * 0.80, total_demand_per_factory * 1.20, size=(nmb_factories,))
     dataset: Dataset = Dataset(nmb_products, nmb_factories, qualification_matrix, qualification_costs, lost_sales_cost,
                                factory_capacities)
 
